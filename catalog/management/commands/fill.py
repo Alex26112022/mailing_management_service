e8f1fd6476e6e868f46bebf640aa49ce8ddf1c67
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-06-04 21:37:13 +0300

    refactoring

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -2,7 +2,7 @@ import json
 
 from django.core.management import BaseCommand
 
-from catalog.models import Product, Category
+from catalog.models import Product, Category, Contacts
 from my_config import path_fixture_json
 
 
@@ -31,12 +31,25 @@ class Command(BaseCommand):
                 list_products.append(el)
         return list_products
 
+    @staticmethod
+    def json_read_contacts():
+        """ Получает данные из фикстур с контактами. """
+        with open(path_fixture_json, encoding='utf-8') as f_cat:
+            data_cat = json.load(f_cat)
+        list_contacts = []
+        for el in data_cat:
+            if el['model'] == "catalog.contacts":
+                list_contacts.append(el)
+        return list_contacts
+
     def handle(self, *args, **options):
         Product.objects.all().delete()
         Category.objects.all().delete()
+        Contacts.objects.all().delete()
 
         product_for_create = []
         category_for_create = []
+        contact_for_create = []
 
         for category in self.json_read_categories():
             category_for_create.append(Category(pk=category.get('pk'),
@@ -70,3 +83,14 @@ class Command(BaseCommand):
                                                   'fields').get('updated_at')))
 
         Product.objects.bulk_create(product_for_create)
+
+        for contact in self.json_read_contacts():
+            contact_for_create.append(Contacts(pk=contact.get('pk'),
+                                               title=contact.get(
+                                                   'fields').get('title'),
+                                               address=contact.get(
+                                                   'fields').get('address'),
+                                               phone=contact.get(
+                                                   'fields').get('phone')))
+
+        Contacts.objects.bulk_create(contact_for_create)
