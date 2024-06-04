b70acf556b62f56096d7810f8e4c1784f680b1bc
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-06-04 12:08:35 +0300

    add admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Product, Category
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'image', 'category', 'price',
+                    'created_at', 'updated_at')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
