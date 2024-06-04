4d7fcd051f3e357cffeeb92b64c065562562ae0c
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-06-04 11:55:11 +0300

    add models

--- config/urls.py
+++ config/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('', include('catalog.urls', namespace='catalog'))
 ]
 
-if settings.DUBUG:
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
