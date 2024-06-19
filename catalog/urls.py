2ab30af0046062737f2454274667cd0a6e5e292f
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-06-19 18:04:38 +0300

    CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import index, contacts, get_product, ProductCreateView
+from catalog.views import ProductCreateView, \
+    ProductListView, ContactlistView, ProductDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('contacts/', contacts, name='contacts'),
-    path('product/<int:pk>/', get_product, name='get_product'),
+    path('', ProductListView.as_view(), name='index'),
+    path('contacts/', ContactlistView.as_view(), name='contacts'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='get_product'),
     path('add_product/', ProductCreateView.as_view(), name='add_product')
 ]
