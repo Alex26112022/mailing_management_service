634eda4ed6ec70b34ef7a67c25d66904ea33b148
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-06-20 23:13:39 +0300

    add blog_data.json

--- my_config.py
+++ my_config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 ROOT_DIR = os.path.dirname(__file__)
 path_fixture_json = os.path.join(ROOT_DIR, 'fixtures', 'catalog_data.json')
+path_blog_fixture_json = os.path.join(ROOT_DIR, 'fixtures', 'blog_data.json')
 
 load_dotenv()
 
