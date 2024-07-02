4a9faac0cb830cf421ee276adeeaa387a39f767f
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-07-02 19:12:55 +0300

    validation form

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, forms
 
 from .models import Product
 
@@ -7,3 +7,12 @@ class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = ('title', 'description', 'image', 'category', 'price')
+
+    def clean_title(self):
+        """ Проверка на запрещенное название. """
+        ban_list = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево',
+                    'бесплатно', 'обман', 'полиция', 'радар']
+        title = self.cleaned_data.get('title')
+        if title.lower() in ban_list:
+            raise forms.ValidationError('Запрещенное название!')
+        return title
