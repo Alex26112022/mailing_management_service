e79f6017b412e4f2c87388f17e5d8b8b03827488
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-07-10 15:02:21 +0300

    reset password

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, \
+    PasswordResetDoneView
 from django.urls import path
 
 from users.apps import UsersConfig
 from users.utils import email_verification
-from users.views import UserCreate, notification
+from users.views import UserCreate, notification, MyPasswordReset
 
 app_name = UsersConfig.name
 
@@ -15,4 +16,8 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'),
          name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('password_reset/', MyPasswordReset.as_view(), name='password_reset'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(
+        template_name='users/password_reset_done.html'),
+         name='password_reset_done')
 ]
--- users/views.py
+++ users/views.py
@@ -1,13 +1,17 @@
 import secrets
 
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.views import PasswordResetView
 from django.core.mail import send_mail
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView
 
 from config.settings import EMAIL_HOST_USER
 from users.forms import UserRegisterForm
+from users.utils import generate_password
 
 
 def notification(request):
@@ -27,7 +31,6 @@ class UserCreate(CreateView):
         user.token = token
         user.save()
         host = self.request.get_host()
-        print(host)
         url = f'http://{host}/users/email-confirm/{token}/'
         send_mail(subject='Подтверждение почты',
                   message=f'Пожалуйста, подтвердите вашу почту. '
@@ -37,3 +40,22 @@ class UserCreate(CreateView):
         return super().form_valid(form)
 
 
+class MyPasswordReset(PasswordResetView):
+    """ Сброс пароля пользователей. """
+    template_name = 'users/password_reset_form.html'
+
+    def form_valid(self, form):
+        email = form.cleaned_data['email']
+        user = get_object_or_404(get_user_model(), email=email)
+        new_password = generate_password(8)
+        hash_new_password = make_password(new_password)
+        user.password = hash_new_password
+        user.save()
+        send_mail(subject='Сброс пароля',
+                  message='',
+                  from_email=EMAIL_HOST_USER,
+                  recipient_list=[user.email],
+                  html_message=f'<h3>Старый пароль успешно сброшен. '
+                               f'Ваш новый пароль: <h2><b>{new_password}</b></h2></h3>')
+
+        return HttpResponseRedirect(reverse('users:password_reset_done'))
